84c1916b34d793522fcb5a532f51573de4933b4c
Kalx6 <kalidabdu921@gmail.com>
2026-05-30 14:35:58 +0300

    update: update env file

--- backend/db/db.config.js
+++ backend/db/db.config.js
@@ -1,11 +1,6 @@
 import mysql from "mysql2/promise";
 import "dotenv/config";
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 export default db;
